f692a8f1c4b3c4c9eb37d541accc07d5fcf84771
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-02-10 15:05:36 +0100

    Exercise 78 - Promise Errors

---
+++ exercise.js
@@ -0,0 +1,47 @@
+const isLogged = true;
+
+
+let firstPromise = (isLogged) => {
+    return new Promise((resolve, reject) => {
+        if (isLogged) {
+        const randomNumber = Math.random();
+        resolve(randomNumber);
+        } else {
+        reject(new Error("Not"));
+        }
+    });
+};
+
+firstPromise(isLogged)
+    .then((randomNumber) => {
+        console.log(`The number is ${randomNumber}`);
+    }).catch((error) => {
+        console.log(error);
+    }
+).finally(() => {
+    console.log('First promise finished');
+    });
+
+
+let number = 0.6;
+
+let secondPromise = (number) => {
+    return new Promise((resolve, reject) => {
+        if (number > 0.5) {
+        resolve({ name: "John", age: 24 });
+            } else {
+        reject(new Error("Number is not greater than 0.5"));
+        }
+    });
+    };
+
+secondPromise(number)
+    .then((object) => {
+        console.log(object);
+    }
+    ).catch((error) => {
+        console.log(error);
+    }
+).finally(() => {
+    console.log('Second promise finished');
+    });
