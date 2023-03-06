c389777d04945c163d2bcca67ff164945c068273
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-06 20:31:06 +0100

    Components - 06

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Hello() {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>What a beautiful day!</p>
+    </div>
+  );
+}
+
+export default Hello;
+
