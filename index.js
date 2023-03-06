0daf5296a68ece374d56d0c9f6ee49d82125e9f0
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-06 19:57:22 +0100

    Components - 02

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Hello from "./App";
+
+function App() {
+  return (
+    <div>
+      <Hello />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+
