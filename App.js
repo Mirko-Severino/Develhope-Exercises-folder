c725f42c3bd9f5e3d649715f071ef3bd0c2cc648
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-07 10:30:17 +0100

    Props - 05

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+class Age extends React.Component {
+  render() {
+    const { age } = this.props;
+    return <span>{age}</span>;
+  }
+}
+
+export default Age;
+
+
+
+
+
+
+
---
+++ index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Age from "./App";
+
+ReactDOM.render(<Age age={30} />, document.getElementById('root'));
