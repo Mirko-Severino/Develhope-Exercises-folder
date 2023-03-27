43174b196956c5c108355eb6b544a92993ccdf5a
Mirko Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-28 01:30:37 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, { Component } from "react";
+import Hello from "./Hello";
 
-function Hello() {
-  return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
-  )
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Hello />
+      </div>
+    );
+  }
 }
-
-export default Hello;
