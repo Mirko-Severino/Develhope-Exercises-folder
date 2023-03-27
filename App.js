81a4d378612963ff713241e9c522c3c5070ab9ee
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-27 23:10:38 +0200

    useEffect - 03

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import GithubUser from "./GitHub";
+
+export default function App() {
+  return (
+    <div>
+      <GithubUser username="Mirko-Severino" />
+    </div>
+  );
+}
