b0f369a842ee888d91a03a290867208aa40a7f8b
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-27 17:51:30 +0200

    Lists - 03

---
+++ Todolist.js
@@ -0,0 +1,34 @@
+import React, { Component, createRef } from "react";
+
+export default class TodoList extends Component {
+  state = {
+    items: [],
+  };
+
+  formRef = createRef();
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { todo } = this.formRef.current.elements;
+    if (todo.value !== "") {
+      this.setState((state) => ({ items: [...state.items, todo.value] }));
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <h1>To Do List</h1>
+        <form ref={this.formRef} onSubmit={this.handleSubmit}>
+          <input name="todo" />
+          <button>+</button>
+        </form>
+        <ul>
+          {this.state.items.map((item, index) => (
+            <li key={item + index}>{item}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
