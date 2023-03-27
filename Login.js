caccdad70cef51b1fc801291e4ddce3ed37d1ba7
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-27 17:43:35 +0200

    Forms - 05

---
+++ Login.js
@@ -0,0 +1,52 @@
+import React, { Component, createRef } from "react";
+
+export default class UncontrolledLogin extends Component {
+  formRef = createRef();
+
+  handleFrom = (e) => {
+    e.preventDefault();
+    const { username, password, remember } = this.formRef.current.elements;
+
+    this.props.onLogin({
+      username: username.value,
+      password: password.value,
+      remember: remember.checked ? true : false,
+    });
+  };
+
+  handleLoginButton = () => {
+    const { username, password, submit } = this.formRef.current.elements;
+
+    if (username.value === "" || password.value === "") {
+      submit.disabled = true;
+    } else {
+      submit.disabled = false;
+    }
+  };
+
+  render() {
+    return (
+      <form ref={this.formRef} onSubmit={this.handleFrom}>
+        <h1>Login</h1>
+        <label>Username:</label>
+        <br />
+        <input name="username" onChange={this.handleLoginButton} />
+        <br />
+        <label>Password:</label>
+        <br />
+        <input
+          name="password"
+          type="password"
+          onChange={this.handleLoginButton}
+        />
+        <br />
+        <input name="remember" type="checkbox" /> <label>Remember</label>
+        <br />
+        <button type="submit" name="submit" disabled={true}>
+          Login
+        </button>
+        <button type="reset">Reset</button>
+      </form>
+    );
+  }
+}
