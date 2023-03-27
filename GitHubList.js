f20348b5570653f839087a15b6cef3ff91a72d69
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-28 01:10:24 +0200

    React Router - 07

---
+++ GitHubList.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+export default function GithubUserList() {
+  const [usernames, setUsernames] = useState([]);
+
+  const handleAddUsername = (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+
+    setUsernames((prevUsernames) => [...prevUsernames, username]);
+  };
+
+  return (
+    <div>
+      <h2>Aggiungi un utente</h2>
+      <form onSubmit={handleAddUsername}>
+        <input name="username" placeholder="Inserisci username" />
+        <button>Aggiungi</button>
+      </form>
+      <ul>
+        {usernames.map((username, index) => (
+          <li key={index + username}>
+            <Link to={`/users/${username}`}>{username}</Link>
+          </li>
+        ))}
+      </ul>
+
+      <Outlet />
+    </div>
+  );
+}
