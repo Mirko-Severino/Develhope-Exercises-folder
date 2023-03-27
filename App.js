75bbc358a1cfea3ec320869c3f2ada06a3d8d925
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-28 00:53:47 +0200

    React Router - 04

---
+++ App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, Routes, Route } from "react-router-dom";
+import ShowGithubUser from "./ShowGithubUser";
+import Welcome from "./Welcome";
+import Counter from "./Counter";
+
+export default function App() {
+  return (
+    <div>
+      <Link to="/">Welcome</Link> | <Link to="/counter">Counter</Link> |{" "}
+      <Link to="/users/Mirko-Severino">Users: Mirko-Severino</Link>
+      <Routes>
+        <Route path="/" element={<Welcome name="Mirko" />} />
+        <Route
+          path="/counter"
+          element={<Counter initialValue={0} increment={1} interval={1000} />}
+        />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
+      </Routes>
+    </div>
+  );
+}
