e1b03ee39c0cc54bcad8c0a45da5eec9d01cd2bb
Mirko-Severino <119595469+Mirko-Severino@users.noreply.github.com>
2023-03-30 13:07:08 +0200

    Type aliases

---
+++ exercise-4-type-aliases-start.ts
@@ -0,0 +1,12 @@
+// Type alias definition
+type Country = string;
+
+// Variables with Country type
+const country1: Country = "Nigeria";
+
+const country2: Country = "Italy";
+
+const country3: Country = "China";
+
+// Export statement to avoid the error: "Cannot redeclare block-scoped variable 'name'."
+export {};
